ee9afc57b1c9e48650b60b3a8933787cd8f04706
Lev <lev-2222@yandex.ru>
2021-01-03 17:35:21 +0300

    initial

---
+++ src/test/java/ru/bot/workbot/WorkbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bot.workbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
