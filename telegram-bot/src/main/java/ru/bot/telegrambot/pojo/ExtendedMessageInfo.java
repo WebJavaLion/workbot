afeebeb0937b44c3682c23f5ce2571161364d266
Lev <lev-2222@yandex.ru>
2021-01-08 07:22:00 +0300

    добавлено поле сессии

--- telegram-bot/src/main/java/ru/bot/telegrambot/pojo/ExtendedMessageInfo.java
+++ telegram-bot/src/main/java/ru/bot/telegrambot/pojo/ExtendedMessageInfo.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import ru.bot.telegrambot.tables.pojos.Session;
 import ru.bot.telegrambot.tables.pojos.UserInfo;
 
 @Data
@@ -15,6 +16,7 @@ import ru.bot.telegrambot.tables.pojos.UserInfo;
 public class ExtendedMessageInfo {
 
     private UserInfo userInfo;
+    private Session userSession;
     private Message wrappedMessage;
     private MessageType messageType;
     private CallbackQuery callbackQuery;
