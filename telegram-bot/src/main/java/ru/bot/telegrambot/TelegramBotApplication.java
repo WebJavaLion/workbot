cca2b7b0113fe67bbf67adaa19471b878a190c32
Padrition <padrition@gmail.com>
2021-03-11 17:27:23 +0600

    рабочий feignClient

--- telegram-bot/src/main/java/ru/bot/telegrambot/TelegramBotApplication.java
+++ telegram-bot/src/main/java/ru/bot/telegrambot/TelegramBotApplication.java
@@ -1,9 +1,14 @@
 package ru.bot.telegrambot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -16,6 +21,9 @@ import ru.bot.telegrambot.service.BotService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(BotProperties.class)
+@EnableDiscoveryClient
+@EnableFeignClients
+@Slf4j
 public class TelegramBotApplication {
 
     public static void main(String[] args) {
@@ -26,5 +34,15 @@ public class TelegramBotApplication {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+
+        final TestService testService = context.getBean(TestService.class);
+
+        log.info(testService.addNewVacancies());
+    }
+
+    @FeignClient("vkstealer")
+    static interface TestService{
+        @GetMapping("/addvacancies")
+        public String addNewVacancies();
     }
 }
